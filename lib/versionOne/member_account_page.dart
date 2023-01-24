a899906d3e55c70a0c81312d9ebf617c084296d4
Samjay <barnabassampana89@gmail.com>
2023-01-24 12:26:55 +0000

    profile on working progress

--- lib/versionOne/member_account_page.dart
+++ lib/versionOne/member_account_page.dart
@@ -4,6 +4,10 @@ import 'package:akwaaba/utils/dimens.dart';
 import 'package:akwaaba/utils/widget_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import '../components/custom_cached_image_widget.dart';
+import '../providers/member_provider.dart';
 
 class MemberAccountPage extends StatefulWidget {
   const MemberAccountPage({Key? key}) : super(key: key);
@@ -18,93 +22,109 @@ class _MemberAccountPageState extends State<MemberAccountPage> {
 
   @override
   Widget build(BuildContext context) {
+    var data = Provider.of<MemberProvider>(context,listen: false).clientAccountInfo;
+    var region = data.region;
+    debugPrint('CLIENT ACCOUTN INFO ${region}');
     return Scaffold(
       extendBodyBehindAppBar: true,
-      body: NestedScrollView(
-        headerSliverBuilder: (context,value){
-          return [ //header
-            SliverAppBar(
-              // backgroundColor: primaryColor,
-              systemOverlayStyle: const SystemUiOverlayStyle(
-                statusBarColor: Colors.transparent,
-                // statusBarIconBrightness: Brightness.light, // For Android (dark icons)
-                // statusBarBrightness: Brightness.dark, // For iOS (dark icons)
-              ),
-              flexibleSpace: FlexibleSpaceBar(
-                background:  Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    headerView()
-                  ],
+      body: Consumer<MemberProvider>(
+          builder: (context, data, child)
+    {
+      return NestedScrollView(
+          headerSliverBuilder: (context, value) {
+            return [ //header
+              SliverAppBar(
+                // backgroundColor: primaryColor,
+                systemOverlayStyle: const SystemUiOverlayStyle(
+                  statusBarColor: Colors.transparent,
+                  // statusBarIconBrightness: Brightness.light, // For Android (dark icons)
+                  // statusBarBrightness: Brightness.dark, // For iOS (dark icons)
                 ),
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      headerView(
+                          profileImage: data.clientAccountInfo.profilePicture)
+                    ],
+                  ),
+                ),
+                automaticallyImplyLeading: true,
+                floating: true,
+                pinned: true,
+                expandedHeight: 300,
               ),
-              automaticallyImplyLeading: true,
-              floating: true,
-              pinned: true,
-              expandedHeight: 300,
+            ];
+          },
+          body: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              //padding: EdgeInsets.all(16),
+              children: [
+                Container(
+                  height: dividerHeight,
+                  color: dividerColor,
+                ),
+                profileItemView(title: "Date of Birth",
+                    label: "${data.clientAccountInfo.applicantGender}"),
+                profileItemView(title: "Gender",
+                    label: data.clientAccountInfo.applicantGender == 1
+                        ? 'Male'
+                        : 'Female',
+                    display: true),
+                profileItemView(
+                    title: "Profession", label: "N/A", display: true),
+                profileItemView(
+                    title: "Place of work", label: "N/A", display: true),
+                profileItemView(title: "Date Joined", label: "N/A"),
+                const SizedBox(height: 24,),
+                Container(
+                  height: dividerHeight,
+                  color: dividerColor,
+                ),
+                const SizedBox(height: 24,),
+
+                const Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: Text("Group", style: TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w600),),
+                ),
+                profileItemView(title: "Group", label: "Senior Staff"),
+                profileItemView(title: "Sub Group", label: "N/A"),
+
+                const SizedBox(height: 24,),
+                Container(
+                  height: dividerHeight,
+                  color: dividerColor,
+                ),
+                const SizedBox(height: 24,),
+
+
+                const Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: Text("Location", style: TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w600),),
+                ),
+                profileItemView(title: "Region", label: "Region"),
+                profileItemView(title: "District", label: "N/A"),
+                profileItemView(title: "Constituency", label: "N/A"),
+                profileItemView(title: "Electoral Area", label: "N/A"),
+                profileItemView(title: "Community", label: "N/A"),
+                profileItemView(title: "Home Town", label: "N/A"),
+                profileItemView(title: "Residential Area", label: "N/A"),
+                profileItemView(title: "Digital Address", label: "N/A"),
+
+                const SizedBox(height: 24,)
+              ],
             ),
-          ];
-        },
-        body:SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            //padding: EdgeInsets.all(16),
-            children: [
-              Container(
-                height: dividerHeight,
-                color: dividerColor,
-              ),
-              profileItemView(title: "Date of Birth", label: "DOB"),
-              profileItemView(title: "Gender", label: "Male",display: true),
-              profileItemView(title: "Profession", label: "N/A",display: true),
-              profileItemView(title: "Place of work", label: "N/A",display: true),
-              profileItemView(title: "Date Joined", label: "N/A"),
-              const SizedBox(height: 24,),
-              Container(
-                height: dividerHeight,
-                color: dividerColor,
-              ),
-              const SizedBox(height: 24,),
-
-              const Padding(
-                padding: EdgeInsets.only(left: 16),
-                child: Text("Group",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
-              ),
-              profileItemView(title: "Group", label: "Senior Staff"),
-              profileItemView(title: "Sub Group", label: "N/A"),
-
-              const SizedBox(height: 24,),
-              Container(
-                height: dividerHeight,
-                color: dividerColor,
-              ),
-              const SizedBox(height: 24,),
-
-
-
-              const Padding(
-                padding: EdgeInsets.only(left: 16),
-                child: Text("Location",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
-              ),
-              profileItemView(title: "Region", label: "Region"),
-              profileItemView(title: "District", label: "N/A"),
-              profileItemView(title: "Constituency", label: "N/A"),
-              profileItemView(title: "Electoral Area", label: "N/A"),
-              profileItemView(title: "Community", label: "N/A"),
-              profileItemView(title: "Home Town", label: "N/A"),
-              profileItemView(title: "Residential Area", label: "N/A"),
-              profileItemView(title: "Digital Address", label: "N/A"),
-
-              const SizedBox(height: 24,)
-            ],
           ),
-        ) ,
-      ),
+      );
+    })
     );
   }
 
-  Widget headerView(){
+  Widget headerView({profileImage}){
     return  Container(
       color: backgroundColor,
       child: Column(
@@ -129,7 +149,19 @@ class _MemberAccountPageState extends State<MemberAccountPage> {
                 ),
                 Positioned(
                     top: 70,
-                    child:defaultProfilePic(height: 100)
+                    child:GestureDetector(
+                      child: profileImage != null
+                          ? Align(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(70),
+                          child: CustomCachedImageWidget(
+                            url: profileImage,
+                            height: 130,
+                          ),
+                        ),
+                      )
+                          : defaultProfilePic(height: 130),
+                    ),
                 )
               ],
             ),
